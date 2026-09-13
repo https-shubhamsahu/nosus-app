ca77330b08b7307e660fb02c2caf645da28d907c
https-shubhamsahu <164779705+https-shubhamsahu@users.noreply.github.com>
2026-09-13 19:21:31 +0000

    Deploying to gh-pages from @ https-shubhamsahu/NON_SUS@c65249b22a3da94548b915a2e248db2a1477042b 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "721261146" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3435207327" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
